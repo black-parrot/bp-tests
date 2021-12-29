10ab0e5b7127b08462c633f12a221cf4e7a54d47
Dan Petrisko <petrisko@cs.washington.edu>
2021-12-29 03:34:56 -0600

    Fixing nonblocking AMO and aviary rom tests (#25)

--- src/amo_nonblocking.c
+++ src/amo_nonblocking.c
@@ -5,8 +5,19 @@
 uint64_t amo_target __attribute__ ((aligned (16))) = 0;
 
 void main(uint64_t argc, char * argv[]) {
-  for (int i = 0; i < 10000; i++) {
-    __asm__ __volatile__ ("amoadd.d x0, %0, 0(%1)" : : "r" (1), "r" (&amo_target));
+  for (int i = 0; i < 1000; i++) {
+    uint64_t amo_target_addr = &amo_target;
+    __asm__ __volatile__ ("amoadd.d x0, %0, 0(%1); \
+                           amoadd.d x0, %0, 0(%1); \
+                           amoadd.d x0, %0, 0(%1); \
+                           amoadd.d x0, %0, 0(%1); \
+                           amoadd.d x0, %0, 0(%1); \
+                           amoadd.d x0, %0, 0(%1); \
+                           amoadd.d x0, %0, 0(%1); \
+                           amoadd.d x0, %0, 0(%1); \
+                           amoadd.d x0, %0, 0(%1); \
+                           amoadd.d x0, %0, 0(%1);"
+                           : : "r" (1), "r" (amo_target_addr));
   }
 
   if (amo_target == 10000) {
