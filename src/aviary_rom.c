37cce7301a864eea7baac54ba0169883bc21e575
Dan Petrisko <petrisko@cs.washington.edu>
2022-01-02 03:42:05 -0800

    Update aviary

--- src/aviary_rom.c
+++ src/aviary_rom.c
@@ -34,11 +34,11 @@ int main(uint64_t argc, char * argv[]) {
     if (bp_param_get(PARAM_BOOT_PC)       != 0x80000000) return -1;
     if (bp_param_get(PARAM_BOOT_IN_DEBUG) != 0         ) return -1;
 
-    if (bp_param_get(PARAM_BRANCH_METADATA_FWD_WIDTH) != 36) return -1;
+    if (bp_param_get(PARAM_BRANCH_METADATA_FWD_WIDTH) != 39) return -1;
     if (bp_param_get(PARAM_BTB_TAG_WIDTH)             != 9 ) return -1;
     if (bp_param_get(PARAM_BTB_IDX_WIDTH)             != 6 ) return -1;
-    if (bp_param_get(PARAM_BHT_IDX_WIDTH)             != 8 ) return -1;
-    if (bp_param_get(PARAM_BHT_ROW_ELS)               != 2 ) return -1;
+    if (bp_param_get(PARAM_BHT_IDX_WIDTH)             != 7 ) return -1;
+    if (bp_param_get(PARAM_BHT_ROW_ELS)               != 4 ) return -1;
     if (bp_param_get(PARAM_GHIST_WIDTH)               != 2 ) return -1;
 
     if (bp_param_get(PARAM_ITLB_ELS_4K) != 8) return -1;
@@ -46,25 +46,21 @@ int main(uint64_t argc, char * argv[]) {
     if (bp_param_get(PARAM_ITLB_ELS_1G) != 0) return -1;
     if (bp_param_get(PARAM_DTLB_ELS_1G) != 0) return -1;
 
-    if (bp_param_get(PARAM_LR_SC)                != 1) return -1;
-    if (bp_param_get(PARAM_AMO_SWAP)             != 1) return -1;
-    if (bp_param_get(PARAM_AMO_FETCH_LOGIC)      != 1) return -1;
-    if (bp_param_get(PARAM_AMO_FETCH_ARITHMETIC) != 1) return -1;
-
-    if (bp_param_get(PARAM_L1_WRITETHROUGH)    != 0  ) return -1;
+    if (bp_param_get(PARAM_DCACHE_WRITETHROUGH) != 0  ) return -1;
+    if (bp_param_get(PARAM_DCACHE_AMO_SUPPORT)  != 15  ) return -1;
+    if (bp_param_get(PARAM_DCACHE_ASSOC)        != 8  ) return -1;
+    if (bp_param_get(PARAM_DCACHE_BLOCK_WIDTH)  != 512) return -1;
     if (multicore) {
-        if (bp_param_get(PARAM_L1_COHERENT)        != 1  ) return -1;
+        if (bp_param_get(PARAM_DCACHE_FILL_WIDTH) != 512) return -1;
     } else {
-        if (bp_param_get(PARAM_L1_COHERENT)        != 0  ) return -1;
+        if (bp_param_get(PARAM_DCACHE_FILL_WIDTH) != 64) return -1;
+    }
+    if (multicore) {
+        if (bp_param_get(PARAM_ICACHE_COHERENT) != 1  ) return -1;
+    } else {
+        if (bp_param_get(PARAM_ICACHE_COHERENT) != 0  ) return -1;
     }
     if (bp_param_get(PARAM_DCACHE_SETS)        != 64 ) return -1;
-    if (bp_param_get(PARAM_DCACHE_ASSOC)       != 8  ) return -1;
-    if (bp_param_get(PARAM_DCACHE_BLOCK_WIDTH) != 512) return -1;
-    if (multicore) {
-        if (bp_param_get(PARAM_DCACHE_FILL_WIDTH)  != 512) return -1;
-    } else {
-        if (bp_param_get(PARAM_DCACHE_FILL_WIDTH)  != 64) return -1;
-    }
     if (bp_param_get(PARAM_ICACHE_SETS)        != 64 ) return -1;
     if (bp_param_get(PARAM_ICACHE_ASSOC)       != 8  ) return -1;
     if (bp_param_get(PARAM_ICACHE_BLOCK_WIDTH) != 512) return -1;
@@ -73,12 +69,14 @@ int main(uint64_t argc, char * argv[]) {
     } else {
         if (bp_param_get(PARAM_ICACHE_FILL_WIDTH)  != 64) return -1;
     }
+    if (bp_param_get(PARAM_ACACHE_AMO_SUPPORT) != 0  ) return -1;
     if (bp_param_get(PARAM_ACACHE_SETS)        != 64 ) return -1;
     if (bp_param_get(PARAM_ACACHE_ASSOC)       != 8  ) return -1;
     if (bp_param_get(PARAM_ACACHE_BLOCK_WIDTH) != 512) return -1;
-    if (bp_param_get(PARAM_ACACHE_FILL_WIDTH)  != 64) return -1;
+    if (bp_param_get(PARAM_ACACHE_FILL_WIDTH)  != 64 ) return -1;
 
     if (bp_param_get(PARAM_L2_EN)               != 1  ) return -1;
+    if (bp_param_get(PARAM_L2_AMO_SUPPORT)      != 14 ) return -1;
     if (bp_param_get(PARAM_L2_DATA_WIDTH)       != 64 ) return -1;
     if (bp_param_get(PARAM_L2_SETS)             != 128) return -1;
     if (bp_param_get(PARAM_L2_ASSOC)            != 8  ) return -1;
@@ -88,6 +86,8 @@ int main(uint64_t argc, char * argv[]) {
 
     if (bp_param_get(PARAM_FE_QUEUE_FIFO_ELS) != 8) return -1;
     if (bp_param_get(PARAM_FE_CMD_FIFO_ELS)   != 4) return -1;
+    if (bp_param_get(PARAM_MULDIV_SUPPORT)    != 3) return -1;
+    if (bp_param_get(PARAM_FPU_SUPPORT)       != 1) return -1;
 
     if (bp_param_get(PARAM_ASYNC_COH_CLK)       != 0  ) return -1;
     if (bp_param_get(PARAM_COH_NOC_FLIT_WIDTH)  != 128) return -1;
