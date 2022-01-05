c0ae6099740c3f8c0a47f94bf3bd31fecccfb931
Kaelin Laundry <kaelinl@cs.washington.edu>
2022-01-04 16:54:27 -0800

    ugh im a dumb

--- src/misaligned_instructions_page_fault.c
+++ src/misaligned_instructions_page_fault.c
@@ -23,18 +23,6 @@
 
 #define PAGE_PERMS_ALL PTE_U_LEAF
 
-// #define pa2kva(pa) ((uint64_t)(pa) - DRAM_BASE - MPGSIZE)
-// #define kva2pa(va) ((uint64_t)(va) + DRAM_BASE + MPGSIZE)
-
-// typedef struct {
-//   uint64_t address;
-//   uint64_t cause;
-//   bool present;
-// } fault_history_t;
-
-// #define MAX_FAULT_COUNT 10
-// fault_history_t fault_history[MAX_FAULT_COUNT] = { 0 };
-
 struct fault_info_t {
   uint64_t address;
   uint64_t cause;
@@ -46,9 +34,9 @@ void pop_tf(trapframe_t*);
 
 typedef uint64_t (*test_gadget_t)();
 
-// Sanity check
-static const uint64_t test_0_aligned_execution_across_page_boundary = TEST_BOUNDARY_VADDR(0) - 4;
-// static const uint64_t test_0_tlb_miss_both_halves_gadget_address = TEST_BOUNDARY_VADDR(0) - 2;
+// Sanity check (fully aligned)
+static const uint64_t test_0_aligned_execution_across_page_boundary_gadget_address = TEST_BOUNDARY_VADDR(0) - 4;
+// static const uint64_t test_1_tlb_miss_both_halves_gadget_address = TEST_BOUNDARY_VADDR(0) - 2;
 
 uint64_t pt[NPT][PTES_PER_PT] __attribute__((aligned(PGSIZE)));
 
@@ -78,53 +66,10 @@ static void map_test_page(uint64_t test_page_num, uint64_t leaf_perm) {
     uint64_t aligned_va = DATA_PAGE_VADDR(test_page_num);
     uint64_t aligned_pa = DATA_PAGE_VADDR_TO_PADDR(aligned_va);
 
-    bp_hprint_uint64(aligned_va);
-    bp_cprint('\n');
-    bp_hprint_uint64(aligned_pa);
-    bp_cprint('\n');
-
     l1pt[vpn2(aligned_va)] = ((uint64_t)l2pt >> PGSHIFT << PTE_PPN_SHIFT) | PTE_V;
     l2pt[vpn1(aligned_va)] = ((uint64_t)l3pt >> PGSHIFT << PTE_PPN_SHIFT) | PTE_V;
     l3pt[vpn0(aligned_va)] = ((uint64_t)aligned_pa >> PGSHIFT << PTE_PPN_SHIFT) | leaf_perm;
 }
-
-
-    // bp_hprint_uint64(vpn2(aligned_va));
-    // bp_cprint('\n');
-    // bp_hprint_uint64(vpn1(aligned_va));
-    // bp_cprint('\n');
-    // bp_hprint_uint64(vpn0(aligned_va));
-    // bp_cprint('\n');
-    // bp_cprint('\n');
-
-// static void page_init(uint64_t addr) {
-//   // First virtual 4k page is allocated to the first 4k physical page
-//   // the rest are mapped to the vpn0'th page of the second megapage
-//   addr = (addr/PGSIZE)*PGSIZE;
-//   uint64_t page_pa;
-//   if(addr == 0)
-//     page_pa = DRAM_BASE;
-//   else 
-//     page_pa = DRAM_BASE + MPGSIZE + (PGSIZE * vpn0(addr));
-//   l1pt[vpn2(addr)] = ((uint64_t)kva2pa(l2pt) >> PGSHIFT << PTE_PPN_SHIFT) | PTE_V;
-//   l2pt[vpn1(addr)] = ((uint64_t)kva2pa(l3pt) >> PGSHIFT << PTE_PPN_SHIFT) | PTE_V;
-//   l3pt[vpn0(addr)] = ((uint64_t)page_pa >> PGSHIFT << PTE_PPN_SHIFT) | PTE_U_LEAF; 
-// }
-
-// static void megapage_init(uint64_t addr) {
-//   // Virtual megapages are mapped to the vpn1'th megapage
-//   addr = (addr/MPGSIZE)*MPGSIZE;
-//   uint64_t page_pa = DRAM_BASE + (MPGSIZE * vpn1(addr));
-//   l1pt[vpn2(addr)] = ((uint64_t)kva2pa(l2pt) >> PGSHIFT << PTE_PPN_SHIFT) | PTE_V;
-//   l2pt[vpn1(addr)] = ((uint64_t)page_pa >> PGSHIFT << PTE_PPN_SHIFT) | PTE_U_LEAF;
-// }
-
-// static void gigapage_init(uint64_t addr) {
-//   // All virtual gigapage are mapped to the first gigapage
-//   addr = (addr/GPGSIZE)*GPGSIZE;
-//   uint64_t page_pa = DRAM_BASE;
-//   l1pt[vpn2(addr)] = ((uint64_t)page_pa >> PGSHIFT << PTE_PPN_SHIFT) | PTE_U_LEAF;
-// }
 
 void handle_fault(uint64_t addr, int cause) {
   if (latest_fault_info.present) {
@@ -136,7 +81,7 @@ void handle_fault(uint64_t addr, int cause) {
   latest_fault_info.cause = cause;
   latest_fault_info.present = true;
 
-  // TODO: don't continue?
+  // TODO: modify target pc to return to the "test finished" handler
 }
 
 void handle_trap(trapframe_t* tf) {
@@ -148,33 +93,7 @@ void handle_trap(trapframe_t* tf) {
   pop_tf(tf);
 }
 
-// int main(int argc, char** argv) {
-
-//   uint64_t satp_val = ((uint64_t)l1pt >> PGSHIFT) | ((uint64_t)SATP_MODE_SV39 * (SATP_MODE & ~(SATP_MODE<<1)));
-//   write_csr(satp, satp_val);
-//   set_csr(mstatus, MSTATUS_SUM);
-//   write_csr(stvec, pa2kva(trap_entry));
-//   write_csr(sscratch, pa2kva(read_csr(mscratch)));
-//   write_csr(medeleg,
-//     (1 << CAUSE_FETCH_PAGE_FAULT) |
-//     (1 << CAUSE_LOAD_PAGE_FAULT) |
-//     (1 << CAUSE_STORE_PAGE_FAULT));
-
-//   // Allocate the last megapage to kernel
-//   l1pt[PTES_PER_PT-1] = ((uint64_t)kpt >> PGSHIFT << PTE_PPN_SHIFT) | PTE_V;
-//   kpt[PTES_PER_PT-1] = ((uint64_t)DRAM_BASE >> PGSHIFT << PTE_PPN_SHIFT) | PTE_S_LEAF;
-
-//   trapframe_t tf;
-//   memset(&tf, 0, sizeof(tf));
-//   tf.epc = (uint64_t)userTest - DRAM_BASE;
-//   tf.gpr[2] = PGSIZE - 0x100;
-//   pop_tf(&tf);
-
-//   return 0;  
-// }
-
 void init_vm() {
-  asm volatile ("fence.i");
   // enable memory mappings
   uint64_t satp_val = ((uint64_t)l1pt >> PGSHIFT) | ((uint64_t)SATP_MODE_SV39 * (SATP_MODE & ~(SATP_MODE<<1)));
   write_csr(satp, satp_val);
@@ -185,6 +104,7 @@ void init_vm() {
     (1 << CAUSE_FETCH_PAGE_FAULT) |
     (1 << CAUSE_FETCH_ACCESS));
 
+  // TODO: temp, try all the fences because things don't work
   asm volatile ("sfence.vma");
   asm volatile ("fence");
 
@@ -197,9 +117,12 @@ void execute_and_expect_fault(uint64_t gadget_address, uint64_t expected_address
 }
 
 void execute_and_expect_success(uint64_t gadget_address) {
-
   test_gadget_t gadget_fn = (test_gadget_t)gadget_address;
-  // bp_hprint_uint64(gadget_address);
+
+  bp_print_string("jumping to: ");
+  bp_hprint_uint64((uint64_t)gadget_fn);
+  bp_cprint('\n');
+
   asm volatile ("li a0, 0"); // TODO: introduce a fake param instead?
   uint64_t result = gadget_fn();
   // "end" instruction sequence returns 0x42
@@ -213,16 +136,18 @@ void execute_and_expect_success(uint64_t gadget_address) {
 }
 
 void run_test() {
-  bp_print_string("insn:\n");
-  bp_hprint_uint64(*((volatile uint64_t*)0x0000000090000ffc));
+  bp_print_string("gadget address:");
+  bp_hprint_uint64(test_0_aligned_execution_across_page_boundary_gadget_address);
   bp_cprint('\n');
-  bp_hprint_uint64(test_0_aligned_execution_across_page_boundary);
+  bp_print_string("insn at address: ");
+  bp_hprint_uint64(*((volatile uint64_t*)test_0_aligned_execution_across_page_boundary_gadget_address));
   bp_cprint('\n');
 
+  // TODO: yet another mindless fencei 
   asm volatile ("fence.i");
 
   latest_fault_info = (const struct fault_info_t){ 0 };
-  execute_and_expect_success(test_0_aligned_execution_across_page_boundary);
+  execute_and_expect_success(test_0_aligned_execution_across_page_boundary_gadget_address);
 
   terminate(0); // temp
 }
@@ -235,15 +160,8 @@ void map_test_pair(int test_num, uint64_t first_page_perms, uint64_t second_page
 }
 
 void place_instruction(uint64_t vaddr, uint32_t instruction) {
-  // TODO: do we allow misaligned stores?
-  *((volatile uint64_t*)DATA_PAGE_VADDR_TO_PADDR(vaddr)) = instruction;
-  // uint32_t value = *((volatile uint64_t*)DATA_PAGE_VADDR_TO_PADDR(vaddr));
-  // if (value == instruction) {
-  //   bp_panic("good");
-  // } else {
-  //   bp_panic("bad");
-  // }
-  // *((uint64_t*)DATA_PAGE_VADDR_TO_PADDR(vaddr)) = instruction;
+  // TODO: misaligned store won't work
+  *((volatile uint32_t*)DATA_PAGE_VADDR_TO_PADDR(vaddr)) = instruction;
 }
 
 void place_dummy_instruction(uint64_t vaddr) {
@@ -259,31 +177,20 @@ void place_end_instructions(uint64_t vaddr) {
 int main(int argc, char** argv) {
 
   map_code_page();
-  map_cfg_page(); // TODO: this might not be needed
-
-  // // boot vector page stays where it is, to avoid managing a relocation
-  // map_page(DRAM_BASE, DRAM_BASE, PAGE_SIZE_MEGAPAGE, PAGE_PERMS_ALL);
-  // // first two megapages mapped 1:1 to make cfg regs available at original locations
-  // // TODO: following two break things
-  // map_page(0, 0, PAGE_SIZE_MEGAPAGE, PAGE_PERMS_ALL);
-  // // map_page(MPGSIZE, MPGSIZE, PAGE_SIZE_MEGAPAGE, PAGE_PERMS_ALL);
-
-  bp_print_string("gadget:\n");
-  bp_hprint_uint64(test_0_aligned_execution_across_page_boundary);
-  bp_cprint('\n');
-  bp_hprint_uint64(DATA_PAGE_VADDR_TO_PADDR(test_0_aligned_execution_across_page_boundary));
-  bp_cprint('\n');
+  map_cfg_page(); // TODO: might not use
 
   map_test_pair(0, PAGE_PERMS_ALL, PAGE_PERMS_ALL);
-  place_dummy_instruction(test_0_aligned_execution_across_page_boundary);
-  place_end_instructions(test_0_aligned_execution_across_page_boundary+4);
+  place_dummy_instruction(test_0_aligned_execution_across_page_boundary_gadget_address);
+  place_end_instructions(test_0_aligned_execution_across_page_boundary_gadget_address+4);
+
+  // TODO: remove
   asm volatile ("fence.i");
 
 
   // place_dummy_instruction(test_0_tlb_miss_both_halves_gadget_address);
   // place_end_instructions(test_0_tlb_miss_both_halves_gadget_address+4);
 
-  bp_hprint_uint64(test_0_aligned_execution_across_page_boundary);
+  bp_hprint_uint64(test_0_aligned_execution_across_page_boundary_gadget_address);
   bp_cprint('\n');
 
   init_vm();
