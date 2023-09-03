52ab68566b05e4eaf4770054fe502b3e891e14de
Scott Davidson <scottdavidson1992@gmail.com>
2023-09-03 08:14:35 -0700

    Adding TC that has error in FPGA for an unhandled machine mode trap

--- src/tc.c
+++ src/tc.c
@@ -4,18 +4,25 @@
 #include "bp_asm.h"
 //#include "bp_dma.h"
 
-// BERT-1
+//#define USE_FOR_LOOP
+#define USE_TC
+
     #define I 4
-    #define J 16
-    #define K 16
+    #define J 128
+    #define K 128
+
+// BERT-1
+//    #define I 4
+//    #define J 768
+//    #define K 768
 
 // BERT-2
-//    #define I 1
+//    #define I 4
 //    #define J 3072
 //    #define K 768
 
 // BERT-3
-//    #define I 1
+//    #define I 4
 //    #define J 768
 //    #define K 3072
 
@@ -29,8 +36,8 @@
 
 #define BUF_SIZE (128*128)
 
-volatile uint32_t A        [BUF_SIZE] __attribute__ ((aligned (64))) __attribute__ ((section (".data"))) = {0};
-volatile uint32_t W        [BUF_SIZE] __attribute__ ((aligned (64))) __attribute__ ((section (".data"))) = {0};
+volatile uint32_t A        [BUF_SIZE] __attribute__ ((aligned (64))) __attribute__ ((section (".data"))) = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+volatile uint32_t W        [BUF_SIZE] __attribute__ ((aligned (64))) __attribute__ ((section (".data"))) = {2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17};
 volatile uint32_t expected [BUF_SIZE] __attribute__ ((aligned (64))) __attribute__ ((section (".data"))) = {0};
 volatile uint32_t result   [BUF_SIZE] __attribute__ ((aligned (64))) __attribute__ ((section (".data"))) = {0};
 
@@ -54,6 +61,9 @@ char bp_printf_buf[BP_PRINTF_MAXLEN];
     __asm__ __volatile__ ( \
         ".insn i 0x0b, 0b000, %0, %0, 64\n\t" \
         ".insn i 0x0b, 0b010, %1, %1, 64\n\t" \
+        "nop\n\t" \
+        "nop\n\t" \
+        "nop\n\t" \
         : : "r" (wt), "r" (addr)  \
     );
 
@@ -65,6 +75,8 @@ char bp_printf_buf[BP_PRINTF_MAXLEN];
         "nop\n\t" \
         "nop\n\t" \
         ".insn i 0x0b, 0b010, %1, %1, 64\n\t" \
+        "nop\n\t" \
+        "nop\n\t" \
         "nop\n\t" \
         : : "r" (wt), "r" (addr)  \
     ); \
@@ -86,6 +98,8 @@ char bp_printf_buf[BP_PRINTF_MAXLEN];
         "nop\n\t" \
         "nop\n\t" \
         ".insn i 0x0b, 0b010, %1, %1, 64\n\t" \
+        "nop\n\t" \
+        "nop\n\t" \
         "nop\n\t" \
         : : "r" (wt), "r" (addr)  \
     ); \
@@ -126,50 +140,40 @@ char bp_printf_buf[BP_PRINTF_MAXLEN];
         "nop\n\t" \
         "nop\n\t" \
         "nop\n\t" \
-        "nop\n\t" \
         : : "r" (wt), "r" (addr)  \
     ); \
 })
-
-//#define USE_FOR_LOOP
-#define USE_TC
 
 int main(int argc, char** argv) {
 
     for (int t = 0; t < 2; t++) {
         uint64_t start_cycles = read_csr(mcycle);
-
-            uint32_t *const R_ptr = &result;
+        uint32_t *const R_ptr = &result;
+        uint32_t *const W_ptr = &W;
+        uint32_t *const A_ptr = &A;
 
         #if defined(USE_TC)
-            for (int i = 0; i < I; i++) {
-                for (int j = 0; j < J; j++) {
-
-                    //uint32_t a_idx = j + i*J;
-
-                    for (int k = 0; k < K; k++) {
-                        //uint32_t w_idx = k + j*K;
+            for (int i = 0; i < I_PRIME; i++) {
+                for (int j = 0; j < J_PRIME; j++) {
+                    for (int k = 0; k < K_PRIME; k++) {
+                        uint32_t a_idx = ((uint32_t)A_ptr) + (j + i*J) - 16;
+                        uint32_t w_idx = ((uint32_t)W_ptr) + (k + j*K) - 16;
                         uint32_t r_idx = ((uint32_t)R_ptr) + (k + i*K);
 
-                        //volatile uint32_t *A_ptr = &A[a_idx];
-                        //volatile uint32_t *W_ptr = &W[w_idx];
-                        //A_ptr -= 16;
-                        //W_ptr -= 16;
-
                         tensor_csr_st(0, r_idx);
-                        //matmul1(A_ptr, W_ptr);
+                        matmul1(a_idx, w_idx);
 
             }}}
 
-        #elif defined(USE_FOR_LOOP)
-            for (int i = 0; i < I; i++) {
-            for (int j = 0; j < J; j++) {
-            for (int k = 0; k < K; k++) {
-                uint32_t a_idx = j + i*J;
-                uint32_t w_idx = k + j*K;
-                uint32_t p_idx = k + i*K;
-                result[p_idx] ^= A[a_idx] ^ W[w_idx];
-            }}}
+        //#elif defined(USE_FOR_LOOP)
+        //    for (int i = 0; i < I; i++) {
+        //    for (int j = 0; j < J; j++) {
+        //    for (int k = 0; k < K; k++) {
+        //        uint32_t a_idx = j + i*J;
+        //        uint32_t w_idx = k + j*K;
+        //        uint32_t p_idx = k + i*K;
+        //        result[p_idx] ^= A[a_idx] ^ W[w_idx];
+        //    }}}
 
         #endif
 
