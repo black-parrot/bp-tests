8514b394f7684e9b5a9ec848843c537387e38459
Scott Davidson <scottdavidson1992@gmail.com>
2023-09-11 01:29:31 -0700

    Updates to TC

--- src/tc_data.py
+++ src/tc_data.py
@@ -8,6 +8,7 @@ random.seed(0)
 I = 16
 J = 16
 K = 16
+
 SA_L = SA_H = SA_W = 4
 
 I_PRIME = I // SA_L
@@ -16,73 +17,50 @@ K_PRIME = K // SA_W
 
 A = np.array([[random.randint(0, 0x7FFFFFFF) for _ in range(J)] for _ in range(I)], dtype=np.uint32)
 W = np.array([[random.randint(0, 0x7FFFFFFF) for _ in range(K)] for _ in range(J)], dtype=np.uint32)
+R = np.zeros((I, K), dtype=np.uint32)
 
-full_result = np.zeros((I, K), dtype=np.uint32)
+E = np.zeros((I, K), dtype=np.uint32)
 for i in range(I):
     for k in range(K):
-        for j in range(J):
-            full_result[i][k] ^= A[i][j] ^ W[j][k]
+        E[i,k] = np.bitwise_xor.reduce(A[i,:] ^ W[:,k])
 
-A_rs = gemm_act_block_tensor(raw_act_data=A , array_height=SA_H , array_width=SA_W , accum_els=2*SA_L)
-W_rs = gemm_wt_block_tensor(raw_wt_data=W, array_height=SA_H, array_width=SA_W, accum_els=2*SA_L)
+A_rs = gemm_act_block_tensor(raw_act_data=A, array_height=SA_H, array_width=SA_W, accum_els=2*SA_L)
+W_rs = gemm_wt_block_tensor (raw_wt_data=W,  array_height=SA_H, array_width=SA_W, accum_els=2*SA_L)
+R_rs = gemm_act_block_tensor(raw_act_data=R, array_height=SA_H, array_width=SA_W, accum_els=2*SA_L)
+E_rs = gemm_act_block_tensor(raw_act_data=E, array_height=SA_H, array_width=SA_W, accum_els=2*SA_L)
 
+E_blk_rs = np.zeros((J_PRIME, I_PRIME, K_PRIME, SA_L, SA_W), dtype=np.uint32)
 
-block_result = np.zeros((I_PRIME, K_PRIME, SA_L, SA_W), dtype=np.uint32)
-all_block_result = np.zeros((J_PRIME, I_PRIME, K_PRIME, SA_L, SA_W), dtype=np.uint32)
-
-for kp in range(K_PRIME): # sub-K
-    for jp in range(J_PRIME): # sub-J
+for kp in range(K_PRIME):
+    for jp in range(J_PRIME):
         for ip in range(I_PRIME):
-            A_blk = A_rs[ip,jp,:,:]
-            W_blk = W_rs[jp,kp,:,:]
-            R_blk = np.zeros((SA_L, SA_W), dtype=np.uint32)
-            for i_s in range(SA_L): # sub-I
-                for j_s in range(SA_H): # sub-J
-                    for k_s in range(SA_W): # sub-K
-                        R_blk[i_s][k_s] ^= A_blk[i_s][j_s] ^ W_blk[j_s][k_s]
-            all_block_result[jp,ip,kp,:,:] = R_blk
-            block_result[ip,kp,:,:] = R_blk
-
-unblock_result = gemm_act_unblock_tensor(raw_act_shape=full_result.shape, act_data=block_result, array_height=SA_H, array_width=SA_W, accum_els=2*SA_L)
-
-#assert np.array_equal(full_result, unblock_result)
-
-
-#############################
+            # tensor-core
+            for i_s in range(SA_L):
+                for j_s in range(SA_H):
+                    for k_s in range(SA_W):
+                        E_blk_rs[jp,ip,kp,i_s,k_s] ^= A_rs[ip,jp,i_s,j_s] ^ W_rs[jp,kp,j_s,k_s]
 
 attr = "__attribute__ ((aligned (64))) __attribute__ ((section (\".data\")))"
 
-# USE ME FOR REAL DATA
-A_rs_str = ",".join(map(str,A_rs.flatten().tolist()))
-W_rs_str = ",".join(map(str,W_rs.flatten().tolist()))
-R_rs_str = ",".join(map(str,block_result.flatten().tolist()))
+A_rs_str     = ",".join(map(str,A_rs.flatten().tolist()))
+W_rs_str     = ",".join(map(str,W_rs.flatten().tolist()))
+R_rs_str     = ",".join(map(str,R_rs.flatten().tolist()))
+E_rs_str     = ",".join(map(str,E_rs.flatten().tolist()))
+E_blk_rs_str = ",".join(map(str,E_blk_rs.flatten().tolist()))
 
-# USE ME FOR ZEROs DATA
 #A_rs_str = "0"
 #W_rs_str = "0"
 #R_rs_str = "0"
+#E_rs_str = "0"
 
-#print(f"#ifndef __TC_DATA_H__")
-#print(f"#define __TC_DATA_H__")
-print(f"")
-print(f"// {all_block_result[0,3,3,:,:]}")
-print(f"// {all_block_result[1,3,3,:,:]}")
-print(f"// {all_block_result[2,3,3,:,:]}")
-print(f"// {all_block_result[3,3,3,:,:]}")
-print(f"")
-print(f"")
-print(f"")
 print(f"#define I {I}")
 print(f"#define J {J}")
 print(f"#define K {K}")
-print(f"")
 print(f"#define I_PRIME {I_PRIME}")
 print(f"#define J_PRIME {J_PRIME}")
 print(f"#define K_PRIME {K_PRIME}")
-print(f"")
-print("volatile uint32_t A[%d] %s = {%s};" % (I * J, attr, A_rs_str))
-print("volatile uint32_t W[%d] %s = {%s};" % (J * K, attr, W_rs_str))
-print("volatile uint32_t expected[%d] %s = {%s};" % (I * K, attr, R_rs_str))
-print("volatile uint32_t result[%d] %s;" % (I * K, attr, ))
-print(f"")
-#print(f"#endif // __TC_DATA_H__")
+print("volatile uint32_t A[%d] %s = {%s};"     % (A_rs.size, attr, A_rs_str))
+print("volatile uint32_t W[%d] %s = {%s};"     % (W_rs.size, attr, W_rs_str))
+print("volatile uint32_t R[%d] %s = {%s};"     % (R_rs.size, attr, R_rs_str))
+print("volatile uint32_t E[%d] %s = {%s};"     % (E_rs.size, attr, E_rs_str))
+print("volatile uint32_t E_BLK[%d] %s = {%s};" % (E_blk_rs.size, attr, E_blk_rs_str))
